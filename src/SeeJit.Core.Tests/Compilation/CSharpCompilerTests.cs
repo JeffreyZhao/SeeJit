3d579b4bc10985788821dd6e881585f751b38291
Jeffrey Zhao <jeffz@live.com>
2017-09-11 22:23:03 -0500

    CSharpCompiler

---
+++ src/SeeJit.Core.Tests/Compilation/CSharpCompilerTests.cs
@@ -0,0 +1,33 @@
+﻿namespace SeeJit.Tests.Compilation
+{
+    using System.IO;
+    using SeeJit.Compilation;
+    using Xunit;
+
+    public class CSharpCompilerTests
+    {
+        [Fact]
+        public void CompilePassed()
+        {
+            var code = "class TestClass { void TestMethod() { var i = 0; } }";
+            var errorWriter = new StringWriter();
+
+            Assert.NotNull(CSharpCompiler.Compile("test.dll", code, errorWriter));
+            Assert.Equal("", errorWriter.ToString());
+        }
+
+        [Fact]
+        public void CompileFailed()
+        {
+            var code = "class TestClass { void TestMethod() { var i = 0 } }";
+            var errorWriter = new StringWriter();
+
+            Assert.Null(CSharpCompiler.Compile("test.dll", code, errorWriter));
+
+            var errorMessage = errorWriter.ToString();
+
+            Assert.Contains("Error", errorMessage);
+            Assert.Contains("; expected", errorMessage);
+        }
+    }
+}
---
+++ src/SeeJit.Core/Compilation/CSharpCompiler.cs
@@ -0,0 +1,51 @@
+﻿namespace SeeJit.Compilation
+{
+    using System.IO;
+    using System.Linq;
+    using System.Reflection;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp;
+
+    public class CSharpCompiler
+    {
+        public static SemanticModel Compile(string assemblyName, string code, TextWriter errorWriter)
+        {
+            var syntaxTree = CSharpSyntaxTree.ParseText(code);
+
+            var references = new MetadataReference[]
+            {
+                MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
+                MetadataReference.CreateFromFile(typeof(Enumerable).Assembly.Location)
+            };
+
+            var compilation = CSharpCompilation.Create(
+                assemblyName,
+                new[] { syntaxTree },
+                references,
+                new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+            using (var ms = new MemoryStream())
+            {
+                var result = compilation.Emit(ms);
+
+                if (result.Success)
+                {
+                    Assembly.Load(ms.ToArray());
+
+                    return compilation.GetSemanticModel(syntaxTree);
+                }
+
+                var failures = result.Diagnostics.Where(diagnostic =>
+                    diagnostic.IsWarningAsError ||
+                    diagnostic.Severity == DiagnosticSeverity.Error);
+
+                foreach (var diagnostic in failures)
+                {
+                    errorWriter.WriteLine($"{diagnostic.Severity} {diagnostic.Id}: {diagnostic.GetMessage()}");
+                }
+
+                return null;
+            }
+        }
+    }
+}
